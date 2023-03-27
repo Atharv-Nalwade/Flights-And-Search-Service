322c23dcd44abaa5dfd81e1d04c0a1e53605cde0
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-03-27 19:44:37 +0530

    Added filter to get all cities

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,5 @@
+const { Op } = require('sequelize');
+
 const { City } = require('../models/index');
 
 class CityRepository {
@@ -61,8 +63,20 @@ class CityRepository {
         }
     }
 
-    async getAllCities(){
+    //filter can be empty 
+    // IF empty show all cities else filter will have a name and we show cities starting with that name
+    async getAllCities(filter){ 
         try {
+            if(filter.name){
+                const cities=await City.findAll({
+                    where:{
+                        name:{
+                            [Op.startsWith]:filter.name
+                        }
+                    }
+                });
+                return cities;
+            }
             const cities=await City.findAll();
             return cities;
         } catch (error) {
