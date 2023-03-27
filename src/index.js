e00af69c5dff9a24aa7ea51339c77f26c301cb81
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-03-28 00:06:13 +0530

    test : Code to log all airports along with City from src/index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
 // Importing models for logging all airports
-// const { Airport,City }=require('../src/models/index');  
+const { Airport,City }=require('../src/models/index');  
 
 const setupAndStartServer = async () => {
 
@@ -21,8 +21,10 @@ const setupAndStartServer = async () => {
         console.log(`Server started at ${PORT}`);
 
         // Code to log all airports
-        // const airports=await Airport.findAll();
-        // console.log(airports);
+        const airports=await Airport.findAll({
+            include:City
+        });
+        console.log(airports);
     });
 }
 
