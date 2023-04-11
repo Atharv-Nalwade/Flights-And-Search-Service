72de21d81a8f8cd47f8fc6402c57917805debaaa
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-04-11 10:00:00 +0530

    completed flight update api

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -37,6 +37,27 @@ const create=async (req,res)=>{
     }
 }
 
+const get = async (req,res) =>{
+    try {
+        console.log(req.query);
+        const response = await flightService.getFlight(req.params.id);
+        return res.status(SuccessCodes.OK).json({
+            data: response,
+            success: true,
+            err: {},
+            message: 'Successfully fetched the flight'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to fetch the flight',
+            err: error
+        });
+    }
+}
+
 const getAll = async (req, res) => {
     try {
         console.log(req.query);
@@ -58,7 +79,29 @@ const getAll = async (req, res) => {
     }
 }
 
+const update = async (req, res) => {
+    try {
+        const response = await flightService.updateFlight(req.params.id, req.body);
+        return res.status(SuccessCodes.OK).json({
+            data: response,
+            success: true,
+            err: {},
+            message: 'Successfully updated the flight'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: 'Not able to update the flight',
+            err: error
+        });
+    }
+}
+
 module.exports = {
     create,
-    getAll
+    getAll,
+    get,
+    update
 }
